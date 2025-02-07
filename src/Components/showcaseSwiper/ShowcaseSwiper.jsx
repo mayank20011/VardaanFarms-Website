7e3f6ee7326e6587714910a13867fa77f36a0862
mayank20011 <mayank.behl.mb1@gmail.com>
2025-02-07 15:14:24 +0530

    Added shaow around corner in slides

--- src/Components/showcaseSwiper/ShowcaseSwiper.jsx
+++ src/Components/showcaseSwiper/ShowcaseSwiper.jsx
@@ -9,7 +9,6 @@ import mobileSlide1 from "../../img/mobileSlider1.jpg";
 import mobileSlide2 from "../../img/mobileSlider2.jpg";
 import mobileSlide3 from "../../img/mobileSlider3.jpg";
 function ShowcaseSwiper() {
-
   return (
     <>
       <Swiper
@@ -20,7 +19,7 @@ function ShowcaseSwiper() {
         modules={[Scrollbar, Autoplay]}
         className="w-full bg-slate-100"
       >
-        <SwiperSlide className="">
+        {/* <SwiperSlide className="">
           <img src={img1} alt="" className="w-full h-full cursor-grab hidden md:block" loading="lazy"/>
           <img src={mobileSlide1} alt="" className="w-full h-full cursor-grab md:hidden" loading="lazy"/>
         </SwiperSlide>
@@ -31,6 +30,60 @@ function ShowcaseSwiper() {
         <SwiperSlide>
         <img src={img3} alt="" className="w-full h-full cursor-grab hidden md:block" loading="lazy"/>
         <img src={mobileSlide3} alt="" className="w-full h-full cursor-grab md:hidden" loading="lazy"/>
+        </SwiperSlide> */}
+        <SwiperSlide>
+          <div className="relative w-full h-full">
+            <img
+              src={img1}
+              alt=""
+              className="w-full h-full cursor-grab hidden md:block"
+              loading="lazy"
+            />
+            <img
+              src={mobileSlide1}
+              alt=""
+              className="w-full h-full cursor-grab md:hidden"
+              loading="lazy"
+            />
+            {/* Shadow overlay */}
+            <div className="absolute inset-0 shadow-[inset_0_0_80px_40px_rgba(0,0,0,0.7)] pointer-events-none"></div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="relative w-full h-full">
+            <img
+              src={img2}
+              alt=""
+              className="w-full h-full cursor-grab hidden md:block"
+              loading="lazy"
+            />
+            <img
+              src={mobileSlide2}
+              alt=""
+              className="w-full h-full cursor-grab md:hidden"
+              loading="lazy"
+            />
+            <div className="absolute inset-0 shadow-[inset_0_0_80px_40px_rgba(0,0,0,0.7)] pointer-events-none"></div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="relative w-full h-full">
+            <img
+              src={img3}
+              alt=""
+              className="w-full h-full cursor-grab hidden md:block"
+              loading="lazy"
+            />
+            <img
+              src={mobileSlide3}
+              alt=""
+              className="w-full h-full cursor-grab md:hidden"
+              loading="lazy"
+            />
+            <div className="absolute inset-0 shadow-[inset_0_0_80px_40px_rgba(0,0,0,0.7)] pointer-events-none"></div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
