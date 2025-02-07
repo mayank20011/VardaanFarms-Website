01b0e19895b5afdb0a37f57eb0adecd779593a98
mayank20011 <mayank.behl.mb1@gmail.com>
2025-02-07 13:37:45 +0530

    Pushing for redeployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,6 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const databasee = getDatabase(app);
 
-export default database;
+export default databasee;
