2a2c2e1a7953cd8647733dcb9c1ecb7f2c1c214c
mayank20011 <mayank.behl.mb1@gmail.com>
2025-02-13 12:25:06 +0530

    added name att to whatsapp button

--- src/Components/WhatsappLink/WhatsappLink.jsx
+++ src/Components/WhatsappLink/WhatsappLink.jsx
@@ -9,7 +9,7 @@ function WhatsappLink() {
   }
 
   return (
-    <button className="fixed right-10 z-50 font-bold bg-green-600 p-1 px-2 rounded-full hover:scale-95 transition shadow-md shadow-green-600" style={{top:"80%"}} onClick={openWhatsapp}>
+    <button className="fixed right-10 z-50 font-bold bg-green-600 p-1 px-2 rounded-full hover:scale-95 transition shadow-md shadow-green-600" style={{top:"80%"}} onClick={openWhatsapp} name="whatsappButton">
       <i className="fa-brands fa-whatsapp text-4xl"></i>
     </button>
   )
