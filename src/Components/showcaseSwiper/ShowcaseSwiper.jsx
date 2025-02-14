974a3c3381c10f55206b718761e4fbcb629bfadb
mayank20011 <mayank.behl.mb1@gmail.com>
2025-02-14 10:32:58 +0530

    .

--- src/Components/showcaseSwiper/ShowcaseSwiper.jsx
+++ src/Components/showcaseSwiper/ShowcaseSwiper.jsx
@@ -21,46 +21,6 @@ const ShowcaseSwiper = () => {
         modules={[Scrollbar, Autoplay]}
         className="w-full bg-slate-100"
       >
-        <SwiperSlide className="relative">
-          <div className="relative w-full h-full">
-            <img
-              src={pcSlider1}
-              alt="Cows Grazing in farm"
-              className="w-full h-full cursor-grab hidden md:block brightness-50"
-            />
-            <img
-              src={mobileSlide1}
-              alt="Cow and Calf Staring at camera"
-              className="w-full h-full cursor-grab md:hidden brightness-50"
-            />
-            <div className="absolute inset-0 shadow-[inset_0_0_80px_40px_rgba(0,0,0,0.7)] pointer-events-none"></div>
-          </div>
-          <div className="absolute z-50 top-0 flex flex-col gap-6 h-full w-full justify-end md:justify-center pb-12 md:pb-0">
-            <div className="mx-auto max-w-[1400px] lg:w-3/4 md:w-11/12 px-4 md:px-0">
-              <div className="md:w-1/2 flex flex-col gap-6 tab:w-3/4">
-                <h1
-                  className="text-white text-3xl md:text-5xl font-bold tab:text-4xl"
-                  id="slide1h1"
-                >
-                  Fresh Farm Milk
-                </h1>
-                <p className="text-lg md:text-lg" id="slide1p">
-                  Our cows graze on lush green pastures, producing milk that's
-                  pure and wholesome. Straight from our farm to your table,
-                  enjoy the taste of freshness in every sip.
-                </p>
-                <Link to={"/blogs"}>
-                  <button
-                    id="slide1btn"
-                    className="w-fit border-2 px-4 py-2 shadow-md shadow-white hover:scale-95 transition cursor-pointer"
-                  >
-                    Read Blog
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
 
         <SwiperSlide className="relative">
           <div className="relative w-full h-full">
@@ -137,11 +97,51 @@ const ShowcaseSwiper = () => {
             </div>
           </div>
         </SwiperSlide>
+
+        <SwiperSlide className="relative">
+          <div className="relative w-full h-full">
+            <img
+              src={pcSlider1}
+              alt="Cows Grazing in farm"
+              className="w-full h-full cursor-grab hidden md:block brightness-50"
+            />
+            <img
+              src={mobileSlide1}
+              alt="Cow and Calf Staring at camera"
+              className="w-full h-full cursor-grab md:hidden brightness-50"
+            />
+            <div className="absolute inset-0 shadow-[inset_0_0_80px_40px_rgba(0,0,0,0.7)] pointer-events-none"></div>
+          </div>
+          <div className="absolute z-50 top-0 flex flex-col gap-6 h-full w-full justify-end md:justify-center pb-12 md:pb-0">
+            <div className="mx-auto max-w-[1400px] lg:w-3/4 md:w-11/12 px-4 md:px-0">
+              <div className="md:w-1/2 flex flex-col gap-6 tab:w-3/4">
+                <h1
+                  className="text-white text-3xl md:text-5xl font-bold tab:text-4xl"
+                  id="slide1h1"
+                >
+                  Fresh Farm Milk
+                </h1>
+                <p className="text-lg md:text-lg" id="slide1p">
+                  Our cows graze on lush green pastures, producing milk that's
+                  pure and wholesome. Straight from our farm to your table,
+                  enjoy the taste of freshness in every sip.
+                </p>
+                <Link to={"/blogs"}>
+                  <button
+                    id="slide1btn"
+                    className="w-fit border-2 px-4 py-2 shadow-md shadow-white hover:scale-95 transition cursor-pointer"
+                  >
+                    Read Blog
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        
       </Swiper>
     </>
   );
-}
+};
 
 export default ShowcaseSwiper;
-
-
