f81e14adfefaec8561685dcc4f91497f9030f13c
mayank20011 <mayank.behl.mb1@gmail.com>
2025-02-17 15:42:53 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    minify:'terser',
-    terserOptions:{
-      compress:{
-        drop_console:true,
-      },
-    },
     outDir: 'dist',
     sourcemap: true,
   }
