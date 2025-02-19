2bbe06e7c936e66af4a2b23654d7f1815c022aca
mayank20011 <mayank.behl.mb1@gmail.com>
2025-02-19 11:57:24 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build:{
+//     outDir: 'dist',
+//     sourcemap: true,
+//   }
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import purgecss from "vite-plugin-purgecss";
+
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    outDir: 'dist',
-    sourcemap: true,
-  }
-})
+  plugins: [
+    react(),
+    purgecss({
+      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+      safelist: ["some-needed-class"],
+    }),
+  ],
+});
